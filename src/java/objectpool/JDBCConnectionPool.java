bf368709aef2b7045cbb7a3c7eb976dddadd321b
Qinglian Zeng <zengqinglian@hotmail.com>
2018-08-30 15:21:53 +0100

    Create JDBCConnectionPool.java

---
+++ src/java/objectpool/JDBCConnectionPool.java
@@ -0,0 +1,52 @@
+package objectpool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnectionPool extends ObjectPool<Connection> {
+
+    private String dsn, usr, pwd;
+
+    public JDBCConnectionPool(String driver, String dsn, String usr, String pwd) {
+	super();
+	try {
+	    Class.forName(driver).newInstance();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	this.dsn = dsn;
+	this.usr = usr;
+	this.pwd = pwd;
+    }
+
+    @Override
+    protected Connection create() {
+	try {
+	    return (DriverManager.getConnection(dsn, usr, pwd));
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    return (null);
+	}
+    }
+
+    @Override
+    public boolean validate(Connection c) {
+	try {
+	    return (!((Connection) c).isClosed());
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    return (false);
+	}
+    }
+
+    @Override
+    public void expire(Connection c) {
+	try {
+	    ((Connection) c).close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
